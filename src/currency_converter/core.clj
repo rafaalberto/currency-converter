fef96185c5facd22570a06f768abad38566b0ad8
Rafael Alberto <rafael.alberto1703@gmail.com>
2022-02-23 17:12:24 -0300

    inserted subs to get arguments value

--- src/currency_converter/core.clj
+++ src/currency_converter/core.clj
@@ -1,8 +1,13 @@
 (ns currency-converter.core
   (:gen-class))
 
+(defn values-in [argument]
+  (cond
+    (.startsWith argument "--from=") {:from (subs argument 7)}
+    (.startsWith argument "--to=") {:to (subs argument 5)}
+    :else {}))
+
 (defn -main
   "Calling static main function"
   [& args]
-  (println "We have " (count args) " argument(s)")
-  (println "Arguments are: " args))
+  (println "Arguments are: " (map values-in args)))
