3e4f8080eeecf00a32fbfd8ff336adc9e4bd2d1a
Rafael Alberto <rafael.alberto1703@gmail.com>
2022-02-24 09:48:27 -0300

    set http-client

--- src/currency_converter/core.clj
+++ src/currency_converter/core.clj
@@ -1,12 +1,23 @@
 (ns currency-converter.core
   (:require [clojure.tools.cli :refer [parse-opts]])
+  (:require [clj-http.client :as http-client])
   (:gen-class))
 
 (def options
   [["-f" "--from currency base" "From Currency" :default "BRL"]
    ["-t" "--to currency base" "To Currency" :default "USD"]])
 
+(def api-url "https://free.currconv.com/api/v7/convert")
+
+(defn set-currency [from to]
+  (str from "_" to))
+
+(def api-key "b53bc913399a901b3544")
+
 (defn -main
   "Calling static main function"
   [& args]
-  (println "Arguments are: " (:options (parse-opts args options))))
+  (let [{:keys [from to]} (:options (parse-opts args options))]
+    (prn "Http execute: " (http-client/get api-url
+                                           {:query-params {"q"      (set-currency from to)
+                                                           "apiKey" api-key}}))))
